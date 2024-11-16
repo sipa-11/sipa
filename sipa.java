502efac602113165a85f1ced3570ff3789adf76b
sipa-11 <sipaalawiyah07@gmail.com>
2024-11-16 13:07:39 +0700

    Add files via upload

---
+++ sipa.java
@@ -0,0 +1,17 @@
+
+public class tempat {
+    public static void main(String[] args) {
+        String nama = "sipa alawiyah";
+        System.out.println(sipaalawiyah.lenght());
+    }
+}
+
+public class tempat1 {
+    public static void main(String[] args) {
+
+        String nama1 = "sipa";
+        String nama2 = "";
+        System.out.println(nama1.isEmpty());
+        System.out.println(nama2.isEmpty());
+    }
+}
